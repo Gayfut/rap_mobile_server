c23ba9e29432d6029920d119075fef732323e2c5
Gayfut <gayfut1@gmail.com>
2021-03-11 19:04:00 +0000

    v0.001.06(change permissions, doing server, need comments)

--- rap_mobile/api/views.py
+++ rap_mobile/api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import AlbumSerializer
 from .models import Album
 
 
 class AlbumViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Album.objects.all().order_by('title')
     serializer_class = AlbumSerializer
--- rap_mobile/rap_mobile/settings.py
+++ rap_mobile/rap_mobile/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .tokens import ip_address, secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ff-$ue2lh12a#f@ltg+ore!tv!+=(a-)jn$45z9l*@8$gxu_w('
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ip_address]
 
 
 # Application definition
